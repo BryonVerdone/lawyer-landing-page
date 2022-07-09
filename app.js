a02068448b4a68db1cda0d2c7e80257a02b2218b
BryonVerdone <bryonverdone@gmail.com>
2022-07-08 20:44:56 -0400

    added number counters

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Mobile nav
+
 const mobileBtn = document.getElementById('mobile-btn');
 const mobileNav = document.getElementById('mobile-nav');
 
@@ -12,4 +14,28 @@ const mobileLinks = document.querySelectorAll('.mobile-link');
 
 mobileLinks.forEach((link) => {
   link.addEventListener('click', showNav);
+});
+
+// data counters
+
+const counters = document.querySelectorAll('.num');
+
+const speed = 100000;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+
+    const increment = target / speed;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCount();
 });
